2e17c77fa93390ddbc078e02cc1d5bdde6017e18
davebambrick <dmbambrick@gmail.com>
2016-05-03 16:30:51 -0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -30,10 +30,9 @@ if method_type == 'POST':
 
 elif method_type == 'GET':
 
-        event = form['event_name'].value
         cnx = _mysql.connect(user='amya_davebam', passwd='S3Dc3tdf',db='amya_davebam')
         #access database, pull event and relevant information:
-        query = ("SELECT * FROM calendar_events WHERE event_name='%s'" %event)
+        query = ("SELECT * FROM calendar_events")
         cnx.query(query)
         results = cnx.store_result()
         table = results.fetch_row(maxrows=0,how=0)
@@ -41,56 +40,67 @@ elif method_type == 'GET':
         for row in table:
                 events.append([e.decode('utf-8') if type(e) is bytes else e for e in row])
         if len(events) > 0:
-                print(events)
-                event_name = events[0][2]
-                location = events[0][3]
-                time = events[0][4]
+                for i in range(3,len(events)):
+                        event = events[i]
+                        event_name = event[2]
+                        location = event[3]
+                        time = event[4]
+                        event_info = {}
 
-        
-
-                #sending request to Google Directions API:   
-                home = "Maseeh Hall"
-                mass_loc = location + " Boston, MA"
-                url_home = urllib.parse.quote(home)
-                url_loc = urllib.parse.quote(mass_loc)
-                api_url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=walking&region=us&key=%s" %(url_home,url_loc,api_key)
-                api_resp = urllib.request.urlopen(api_url)
-                api_content = api_resp.read().decode('utf-8')
-                result = json.loads(api_content)
-
-                points = result['geocoded_waypoints']
-                pi_1 = points[0]['place_id']
-                pi_2 = points[1]['place_id']
-                routes = result['routes']
-                status = result['status']
-
-                print(pi_1,pi_2)
-                print(status)
-                if len(routes) > 0:
-                        legs = routes[0]['legs']
-                        duration = legs[0]['duration']
-                        distance = legs[0]['distance']
-                        steps = legs[0]['steps']
-                        duration_seconds = duration['value']
-                        distance_meters = distance['value']
-                        print(duration_seconds, distance_meters)
-                        step_instructions = []
-                        for i in range(len(steps)):
-                                current_step = steps[i]
-                                step_instructions.append(current_step['html_instructions'])
-                        
-
+                        time_obj = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+                        time_delta = datetime.now() - time_obj
+                        if time_delta.total_seconds() <= 86400:
                 
-                time_obj = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
-                time_d = datetime.now() - time_obj
-                duration_mins = duration_seconds/60
+                                #sending request to Google Directions API:   
+                                home = "Maseeh Hall"
+                                mass_loc = location + " Boston, MA"
+                                url_home = urllib.parse.quote(home)
+                                url_loc = urllib.parse.quote(mass_loc)
+                                api_url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=walking&region=us&key=%s" %(url_home,url_loc,api_key)
+                                api_resp = urllib.request.urlopen(api_url)
+                                api_content = api_resp.read().decode('utf-8')
+                                result = json.loads(api_content)
+
+                                points = result['geocoded_waypoints']
+                                pi_1 = points[0]['place_id']
+                                pi_2 = points[1]['place_id']
+                                routes = result['routes']
+                                status = result['status']
+
+                                if len(routes) > 0:
+                                        legs = routes[0]['legs']
+                                        duration = legs[0]['duration']
+                                        distance = legs[0]['distance']
+                                        steps = legs[0]['steps']
+                                        duration_seconds = duration['value']
+                                        distance_meters = distance['value']
+                                        
+                                        step_instructions = []
+                                        for i in range(len(steps)):
+                                                current_step = steps[i]
+                                                step_instructions.append(current_step['html_instructions'])
+                                
+                                        duration_mins = duration_seconds/60
+                                        if time_delta.total_seconds() <= (duration_seconds + 300):
+                                                time_statement = ("It will take you about %.1f minutes to arrive. You should leave for %s now." %(duration_mins, event_name))
+
+                                        event_info["<b>event</b>"] = []
+                                        for i in range(len(event)):
+                                                event_info["<b>event</b>"].append(event[i])
+                                        event_info["<b>duration_seconds</b>"] = duration_seconds
+                                        event_info["<b>distance_meters</b>"] = distance_meters
+                                        event_info["<b>step_instructions</b>"] = step_instructions
+                                        event_info["<b>time_statement</b>"] = time_statement
+
+                                        print(event_info)
+                                        print("<br>")
+
+                                else:
+                                        print("No directions found to %s." %event_name)
                 
-                if time_d.total_seconds() <= duration_seconds:
-                        print("It will take you about %.1f minutes to arrive. You should leave for %s now." %(duration_mins, event_name))
-                	print(step_instructions)
-                	
         else:
                 print("Database Error")
+                
 
         
         
